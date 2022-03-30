77b9c7468bd00c5ce561aba69584aa3ff9d03db6
Harithakumpati <98130767+Harithakumpati@users.noreply.github.com>
2022-03-30 16:44:05 -0500

    added bonus

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
- var times = 0;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  times++;
-  res.send(`User accesses are:${times}`);
+  const x= req.query.x || getRandomArbitrary(0, 60);
+  var y1 = Math.atan2(x);
+  var y2 = Math.atanh(x);
+  var y3 = Math.cbrt(x);
+  function getRandomArbitrary(min, max) {
+    return Math.cbrt(Math.random() * (max - min) + min);
+  }
+  res.send(`Math.atan2(${x}) is ${y1} ; Math.atanh(${x}) is ${y2} ; Math.;cbrt(${x}) is ${y3}\n`);
 });
 
 module.exports = router;
